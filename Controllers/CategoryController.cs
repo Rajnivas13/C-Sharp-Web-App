b07c19de93df964707110a5816a8ad22604663cc
Rajnivas T <rajnivas.t@emsibg.com>
2023-01-18 15:34:06 +0530

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using learningsharp.Data;
+using learningsharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learningsharp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objlist= _db.Category;
+            return View(objlist);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using learningsharp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learningsharp.Data
+{
+    public class ApplicationDbContext :DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
